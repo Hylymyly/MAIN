f4319eafbc369522993259fca4256776f1c4685f
Hylymyly <61086724+Hylymyly@users.noreply.github.com>
2020-04-22 13:51:14 +0300

    Update KTP50.java

--- Zadachi/KTP50.java
+++ Zadachi/KTP50.java
@@ -1,47 +1,94 @@
+
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+import java.security.MessageDigest;
+import java.util.*;
+import java.util.Base64;
 import java.util.LinkedHashSet;
 
 public class KTP50 {
 
-    public static void main(String[] args) {
-        int[] arr = new int[]{72,33,-73,84,-12,-3,13,-13,-68};
-        String[] arr2 = new String[]{"hoops","chuff","bot","bottom"};
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        /*int[] arr = new int[]{72, 33, -73, 84, -12, -3, 13, -13, -68};
+        String[] arr2 = new String[]{"hoops", "chuff", "bot", "bottom" +
+                ""};
         String[] arr3 = sameVowelGroup(arr2);
         //long[] ints = validateCard(1234567890123456L);
-        for (int i =0; i<arr3.length;i++)
-        System.out.print(arr3[i]+" ");
+        for (int i = 0; i < arr3.length; i++)
+            System.out.print(arr3[i] + " ");*/
         //System.out.println(encrypt("Hello"));
         //System.out.println(decrypt(arr));
         //System.out.println(sumDigProd(1,2,3,4,5,6));
         //System.out.println(validateCard(1234567890123452L));
         //System.out.println(numToEng(901));
         //System.out.println(sameVowelGroup(arr2));
+        //System.out.println(canMove("Bishop","A7","G1"));
+        //System.out.println(getSha256Hash("password123"));
+        //System.out.println(correctTitle("jOn SnoW, kINg IN thE noRth."));
     }
 
-    public static int[] encrypt(String s)
-    {
+    public static int[] encrypt(String s) {
         int[] arr = new int[s.length()];
         char firstsym = s.charAt(0);
-        arr[0]=firstsym;
-        for (int i =1; i<s.length();i++)
-        {
-            arr[i]=s.charAt(i-1)-s.charAt(i);
+        arr[0] = firstsym;
+        for (int i = 1; i < s.length(); i++) {
+            arr[i] = s.charAt(i - 1) - s.charAt(i);
         }
         return arr;
     }
 
-    public static String decrypt(int[] arr)
-    {
+    public static String decrypt(int[] arr) {
         int[] arr2 = new int[arr.length];
-        arr2[0]=arr[0];
+        arr2[0] = arr[0];
         String line = "";
-        for (int j=1;j<arr.length;j++)
-        {
-            arr2[j]=arr2[j-1]+arr[j];
+        for (int j = 1; j < arr.length; j++) {
+            arr2[j] = arr2[j - 1] + arr[j];
         }
-        for (int i = 0; i<arr.length;i++) {
-            line += (String.valueOf((char)arr2[i]));
+        for (int i = 0; i < arr.length; i++) {
+            line += (String.valueOf((char) arr2[i]));
         }
         return line;
+    }
+
+    public static boolean canMove(String figure, String cow, String row)
+    {
+        char cowstr = cow.toLowerCase().charAt(0);//letter
+        char rowstr = row.toLowerCase().charAt(0);
+        int cowint = Integer.parseInt(String.valueOf(cow.charAt(1)));//number
+        int rowint = Integer.parseInt(String.valueOf(row.charAt(1)));
+        switch (figure)
+        {
+            case "King":
+            {
+                if ((Math.abs(cowstr-rowstr)<2)&&(Math.abs(cowint-rowint)<2)) return true; else return false;
+            }
+            case "Rook":
+            {
+                if(((cowstr==rowstr)&&(cowint!=rowint))||((cowstr!=rowstr)&&(cowint==rowint))) return true;else return false;
+            }
+            case "Queen":
+            {
+                if(((cowstr!=rowstr)&&(cowint==rowint))||((cowstr==rowstr)&&(cowint!=rowint))) return true;
+                if(Math.abs(cowint-rowint)==Math.abs(cowstr-rowstr)) return true;
+                break;
+            }
+            case "Bishop":
+            {
+                if (Math.abs(cowstr-rowstr)==Math.abs(cowint-rowint)) return true;else return false;
+            }
+            case "Knight":
+            {
+                if(((Math.abs(cowstr-rowstr)==1)&&(Math.abs(cowint-rowint)==2))||((Math.abs(cowstr-rowstr)==2)&&(Math.abs(cowint-rowint)==1))) return true; else return false;
+            }
+            case "Pawn":
+            {
+                if ((cowstr==rowstr)&&(cowint==2)&&(rowint==4)) return true;
+                if ((cowstr==rowstr)&&(cowint==(rowint-1))) return true;
+                return false;
+            }
+            default: return false;
+        }
+        return false;
     }
 
     public static boolean canComplete(String a, String b)
@@ -264,5 +311,33 @@ public class KTP50 {
             }
         }
         return s;
+    }
+
+    public static String getSha256Hash(String str) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] encodedhash = digest.digest(str.getBytes(StandardCharsets.UTF_8));
+        String res = Base64.getEncoder().encodeToString(encodedhash);
+        char[] chars = "0123456789ABCDEFF".toCharArray();
+        char[] chars1 = new char[encodedhash.length*2];
+        for (int i = 0; i < encodedhash.length;i++)
+        {
+            int v = encodedhash[i] & 0xFF;
+            chars1[i*2]=chars[v>>>4];
+            chars1[i*2+1]=chars[v & 0x0F];
+        }
+        return new String(chars1).toLowerCase();
+    }
+
+    public static String correctTitle(String a)
+    {
+        String line ="";
+        String[] input = a.toLowerCase().split(" ");
+        for(String str:input)
+        {
+            String word = String.valueOf(str.charAt(0)).toUpperCase();
+            if(str.equals("and")||str.equals("the")||str.equals("of")||str.equals("in")) line = line + str + " ";
+            else line = line + word +str.substring(1)+" ";
+        }
+        return line;
     }
 }
