66131b299de9a9e5f99ac3c72b13a374681dfea2
Hylymyly <61086724+Hylymyly@users.noreply.github.com>
2021-02-17 19:58:14 +0300

    Protect
    17.02.2021

---
+++ Protect/AntiCLient/AntiCLient/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AntiCLient
+{
+    public partial class Form1 : Form
+    {
+        #region dll
+        [DllImport("kernel32.dll")]
+        static extern IntPtr CreateMutex(IntPtr lpMutexAttributes, bool bInitialOwner, string lpName);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
+        [DllImport("kernel32.dll")]
+        static extern IntPtr CreateEvent(IntPtr lpEventAttributes, bool bManualReset, bool bInitialState, string lpName);
+        [DllImport("kernel32.dll")]
+        static extern bool ResetEvent(IntPtr hEvent);
+        [DllImport("kernel32.dll")]
+        static extern bool SetEvent(IntPtr hEvent);
+        [DllImport("Kernel32.dll", SetLastError = true)]
+        static extern IntPtr OpenEvent(uint dwDesiredAccess, bool bInheritHandle, string lpName);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool CloseHandle(IntPtr hObject);
+        [DllImport("kernel32.dll")]
+        static public extern bool PulseEvent(IntPtr hEvent);
+        [DllImport("user32.dll")]
+        public static extern int GetSystemMetrics(int nIndex);
+        public const int SM_MOUSEPRESENT = 19;
+        public const int SMCMOUSEBUTTONS = 43;
+        public const int SM_MOUSEWHEELPRESENT = 75;
+        #endregion
+        static TcpListener tcpListener = new TcpListener(10);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Listeners();
+            tcpListener.Stop();
+        }
+
+        private void Listeners()
+        {
+            tcpListener.Start();
+            Socket socketForClient = tcpListener.AcceptSocket();
+            if (socketForClient.Connected)
+            {
+                int i = 0;
+                NetworkStream networkStream = new NetworkStream(socketForClient);
+                System.IO.StreamWriter streamWriter = new System.IO.StreamWriter(networkStream);
+                System.IO.StreamReader streamReader = new System.IO.StreamReader(networkStream);
+                string str = "C:\\Users\\yaros\\Desktop\\txt.txt 001";
+                streamWriter.WriteLine(str);
+                streamWriter.Flush();
+                Thread.Sleep(1000);
+                while (i != 1)
+                    {
+                        i++;
+                        string theString = streamReader.ReadLine();
+                        label3.Text = "Сообщение клиента: " + socketForClient.RemoteEndPoint + " " + theString;
+                    }
+                streamReader.Close();
+                networkStream.Close();
+                streamWriter.Close();
+            }
+            socketForClient.Close();
+        }
+    }
+}
---
+++ Protect/AntiServer/AntiServer/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace AntiServer
+{
+    class Program
+    {
+        #region dll
+        [DllImport("kernel32.dll")]
+        static extern bool ReleaseMutex(IntPtr hMutex);
+        [DllImport("kernel32.dll")]
+        static extern IntPtr CreateMutex(IntPtr lpMutexAttributes, bool bInitialOwner, string lpName);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
+        [DllImport("kernel32.dll")]
+        static extern IntPtr CreateEvent(IntPtr lpEventAttributes, bool bManualReset, bool bInitialState, string lpName);
+        [DllImport("kernel32.dll")]
+        static extern bool ResetEvent(IntPtr hEvent);
+        [DllImport("kernel32.dll")]
+        static extern bool SetEvent(IntPtr hEvent);
+        [DllImport("Kernel32.dll", SetLastError = true)]
+        static extern IntPtr OpenEvent(uint dwDesiredAccess, bool bInheritHandle, string lpName);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool CloseHandle(IntPtr hObject);
+        [DllImport("kernel32.dll")]
+        static public extern bool PulseEvent(IntPtr hEvent);
+        #endregion
+        static void Main(string[] args)
+        {
+            TcpClient socketForServer;
+            while (true)
+            {
+                try
+                {
+                    try
+                    {
+                        Console.WriteLine("Ожидание подключение...");
+                        Thread.Sleep(2000);
+
+                        socketForServer = new TcpClient("localHost", 10);
+
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+
+                    NetworkStream networkStream = socketForServer.GetStream();
+                    System.IO.StreamWriter streamWriter = new System.IO.StreamWriter(networkStream);
+                    System.IO.StreamReader streamReader = new System.IO.StreamReader(networkStream);
+                    Console.WriteLine("\r\n*Клиент подключенный по порту №10*");
+                    string strvod = streamReader.ReadLine();
+                    var text = strvod.Split(' ');
+                    string path = text[0];
+                    string code = text[1];
+                    System.IO.StreamReader sr = new System.IO.StreamReader(path);
+                    Console.WriteLine(sr.ReadToEnd());
+                    Console.WriteLine("Проверка");
+                    Thread.Sleep(1000);
+
+
+                    string str = " проверено "+ path + " rhz "+ code;
+
+                    streamWriter.WriteLine(str);
+                    streamWriter.Flush();
+                    networkStream.Close();
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Соединение разорвано\r\n");
+
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка считывания");
+                }
+            }
+        }
+       
+    }
+}
